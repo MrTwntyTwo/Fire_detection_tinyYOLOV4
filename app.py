f1b6ad6504928e8525ddf2d84065aa476144f8c0
muftimiftahul <99190356+MrTwntyTwo@users.noreply.github.com>
2023-10-10 19:58:47 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+
+# Load YOLO
+net = cv2.dnn.readNet("your yolo weights", "your yolo cfg")
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+# output_layers = [net.getUnconnectedOutLayers()]
+# Load class labels
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# Initialize webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read and resize frame from webcam
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=1.6, fy=1.6)
+
+    height, width, channels = frame.shape
+
+    # Detect objects
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Initialize detection variables
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    # Process outputs
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+
+            if confidence > 0.5:            # Set minimum confidence threshold
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    # Apply non-maximum suppression
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    # Draw bounding boxes and labels
+    font = cv2.FONT_HERSHEY_PLAIN
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    if len(indexes) > 0:
+        for i in indexes.flatten():
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = str(round(confidences[i], 2))
+            # Set bounding box color
+            color = (255, 0, 0)
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, f"{label} {confidence}", (x, y + 20), font, 2, (255, 255, 255), 2)
+
+    # Display frame
+    cv2.imshow("Webcam", frame)
+    key = cv2.waitKey(1)
+
+    if key == 27:  # Press 'Esc' to exit
+        break
+
+# Release webcam and close windows
+cap.release()
+cv2.destroyAllWindows()
